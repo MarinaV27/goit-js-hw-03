5d3551fc47f47376b8e658a8c6b5da94a26a9612
MarinaV27 <Vasilishinamari@gmail.com>
2024-01-27 00:24:43 +0200

    task- 1 end

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,16 @@
 function makeArray(firstArray, secondArray, maxLength) {
-   return newArray = firstArray.concat(secondArray);
+   const newArr = [];
+   //const filterArrayIndex = firstArray.length-1;
+   //const firstArrayElement = firstArray[filterArrayIndex];
+   //const secondArrayIndex = secondArray.length-1;
+   //const secondArrayElement = secondArray[secondArrayIndex];
+
+  for (let i = 0; i < firstArray.length; i++) { if  maxLength < firstArrayElement.length { return 
+    newArr.slice(0,maxLengt)
+  }return
+    newArr = firstArray[i];
+    
+  }
     
 }
 
@@ -21,3 +32,12 @@ console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); //
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+
+//Напиши функцію під назвою makeArray, яка приймає три параметри: firstArray (масив), secondArray (масив) і maxLength (число). Функція повинна створювати новий масив, який містить усі елементи з firstArray, а потім усі елементи з secondArray.
+
+//Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути копію масиву з довжиною maxLength елементів.
+//В іншому випадку функція повинна повернути весь новий масив.
+
+
+//Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
